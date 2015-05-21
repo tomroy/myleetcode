d3d851754e5c95ffc8cff2b083ffad0ef042188e
tom.th_lin <tom.th_lin@htc.com>
2015-05-21 14:31:46 +0800

    ADD. Multiply Strings

---
+++ src/com/tomroy/MultiplyStrings.java
@@ -0,0 +1,12 @@
+package com.tomroy;
+
+public class MultiplyStrings {
+
+    public static String multiply(String num1, String num2) {
+        return "1";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply("1","2"));
+    }
+}
